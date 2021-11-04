790da6e6a4dba52d2d4c0de1faeb46abb7b1b3ec
fujiawei <1479028470@qq.com>
2021-11-04 15:10:22 +0800

    Temporary store

---
+++ include/TrajectoryPlanning.hpp
@@ -0,0 +1,18 @@
+/*
+ * @Author: fujiawei0724
+ * @Date: 2021-11-04 15:05:54
+ * @LastEditors: fujiawei0724
+ * @LastEditTime: 2021-11-04 15:08:32
+ * @Descripttion: The components for trajectory planning. 
+ */
+
+#pragma once
+
+#include "Point.hpp"
+
+namespace TrajectoryPlanner {
+
+
+
+} // End of namespace trajectory planner
+
